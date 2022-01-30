c36fc3f5181db6e4f4920658cb0d36b08d7ff8be
Devendra Jadhav <devendrajadhav4@yahoo.co.in>
2022-01-30 17:45:12 +0530

    Initial commit for project

---
+++ src/main/java/com/simplilearn/project/controller/Project3Controller.java
@@ -0,0 +1,14 @@
+package com.simplilearn.project.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Project3Controller {
+
+	@GetMapping("/")
+	public String index() {
+		return "SimpliLearn - DevOps Certification Training - Project 3";
+	}
+
+}
