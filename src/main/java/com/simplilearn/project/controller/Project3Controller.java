fb618492f77bbe1a2980106bfdbfb83a0b6c04e9
Devendra Jadhav <devendrajadhav4@yahoo.co.in>
2022-02-05 12:55:27 +0530

    Changed the return message for demo purposes

--- src/main/java/com/simplilearn/project/controller/Project3Controller.java
+++ src/main/java/com/simplilearn/project/controller/Project3Controller.java
@@ -8,7 +8,7 @@ public class Project3Controller {
 
 	@GetMapping("/")
 	public String index() {
-		return "### SimpliLearn - DevOps Certification Training - Project 3";
+		return "### SimpliLearn - DevOps Certification Training - Project 3 ###";
 	}
 
 }
